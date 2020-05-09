9ea41c7dc5e7b435bb9de9fc2930b63194d4ac1a
Ichunjo <ichunjo.le.terrible@gmail.com>
2020-05-09 17:47:12 +0200

    Retrait du frangrish et des variables memes

--- baguettisation.lua
+++ baguettisation.lua
@@ -32,15 +32,16 @@ function dialogue(subs, sel, styles)
 		if string.sub(line.text, 1, 2) == "- " then
 			line.text = line.text:gsub("- ", "– ")
 			cleantag = line.text:gsub("{[^}]+}", "")
-			xsplit = split(cleantag, "\\N")
-			if xsplit[2] ~= nil then
-				if #xsplit[1] >= #xsplit[2] then
-					lineLaPlusLongue = xsplit[1]
+			split_line = split(cleantag, "\\N")
+			
+			if split_line[2] ~= nil then
+				if #split_line[1] >= #split_line[2] then
+					longest_line = split_line[1]
 				else
-					lineLaPlusLongue = xsplit[2]
+					longest_line = split_line[2]
 				end
 
-				width, height, descent, extlead = aegisub.text_extents(styles[line.style], lineLaPlusLongue)
+				width, height, descent, extlead = aegisub.text_extents(styles[line.style], longest_line)
 				video_x, video_y = aegisub.video_size()
 
 				line.margin_l = (video_x/2) - round((width/2),0)
