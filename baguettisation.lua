242d8bdac6abc012dcff1a03cbfd528ce5d8f5f3
Ichunjo <ichunjo.le.terrible@gmail.com>
2020-05-09 17:58:49 +0200

    Consistence tabulations

--- baguettisation.lua
+++ baguettisation.lua
@@ -12,49 +12,49 @@ function split(s, delimiter)
 end
 
 function round(num, numDecimalPlaces) 
-	mult = 10^(numDecimalPlaces or 0)
-	return math.floor(num * mult + 0.5) / mult
+    mult = 10^(numDecimalPlaces or 0)
+    return math.floor(num * mult + 0.5) / mult
 end
 
 function dialogue(subs, sel, styles)
-	styles = { n = 0 }
-	for i, l in ipairs(subs) do
-		if l.class == "style" then
-			styles.n = styles.n + 1
-			styles[styles.n] = l
-			styles[l.name] = l
-			l.margin_v = l.margin_t
-		end
-	end
+    styles = { n = 0 }
+    for i, l in ipairs(subs) do
+        if l.class == "style" then
+            styles.n = styles.n + 1
+            styles[styles.n] = l
+            styles[l.name] = l
+            l.margin_v = l.margin_t
+        end
+    end
 
     for k, i in ipairs(sel) do
-		line = subs[i]
-		if string.sub(line.text, 1, 2) == "- " then
-			line.text = line.text:gsub("- ", "– ")
-			cleantag = line.text:gsub("{[^}]+}", "")
-			split_line = split(cleantag, "\\N")
-			
-			if split_line[2] ~= nil then
-				if #split_line[1] >= #split_line[2] then
-					longest_line = split_line[1]
-				else
-					longest_line = split_line[2]
-				end
+        line = subs[i]
+        if string.sub(line.text, 1, 2) == "- " then
+            line.text = line.text:gsub("- ", "– ")
+            cleantag = line.text:gsub("{[^}]+}", "")
+            split_line = split(cleantag, "\\N")
+            
+            if split_line[2] ~= nil then
+                if #split_line[1] >= #split_line[2] then
+                    longest_line = split_line[1]
+                else
+                    longest_line = split_line[2]
+                end
 
-				width, height, descent, extlead = aegisub.text_extents(styles[line.style], longest_line)
-				video_x, video_y = aegisub.video_size()
+                width, height, descent, extlead = aegisub.text_extents(styles[line.style], longest_line)
+                video_x, video_y = aegisub.video_size()
 
-				line.margin_l = (video_x/2) - round((width/2),0)
-				line.style = "Default - Dialogue"
-				-- Le Default - Dialogue doit être en an1 !
-				subs[i] = line
-			end
-		end
-	end
+                line.margin_l = (video_x/2) - round((width/2),0)
+                line.style = "Default - Dialogue"
+                -- Le Default - Dialogue doit être en an1 !
+                subs[i] = line
+            end
+        end
+    end
 end
 
 function baguettetisation(subs, sel)
-	dialogue(subs, sel, styles)
+    dialogue(subs, sel, styles)
 end
 
 aegisub.register_macro(script_name,script_description,baguettetisation)
